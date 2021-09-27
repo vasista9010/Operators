04361d0c98f5abb7333ba8c2f647c470b5dca927
radha <vasuvasista15@gmail.com>
2021-09-27 16:32:21 +0530

    operator concepts

---
+++ src/com/vasu/Main.java
@@ -0,0 +1,12 @@
+package com.vasu;
+public class Main {
+    public static void main(String[] args) {
+        int x =10;
+        int y= ++ x;
+        System.out.println(y);
+        int i = 10;
+        int j=20;
+        int k= i+j;
+        System.out.println(k);
+    }
+}
