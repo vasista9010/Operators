a6e61e721453118efdc68407b6d8ca59c021d0e9
radha <vasuvasista15@gmail.com>
2021-09-28 13:12:57 +0530

    operator concepts

---
+++ src/com/vasu/IncrementDecrement.java
@@ -0,0 +1,17 @@
+package com.vasu;
+public class IncrementDecrement {
+    public static void main(String[] args) {
+        int x =10;
+        int y= ++x;
+        System.out.println(y);
+        int x1=10;
+        int y1=x1++;
+        System.out.println(y1);
+        int x3=10;
+        int y3=x3--;
+        System.out.println(y3);
+        int x4=10;
+        int y4=--x4;
+        System.out.println(y4);
+    }
+}
